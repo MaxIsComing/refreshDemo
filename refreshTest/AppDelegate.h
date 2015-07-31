f31c65b48176120234efbdf7ce5586a1de20405b
golven <golven@yunkandeMacBook-Pro.local>
2015-07-31 13:24:07 +0800

    Initial Commit

---
+++ refreshTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  refreshTest
+//
+//  Created by golven on 15/7/31.
+//  Copyright (c) 2015年 magicEngineer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
